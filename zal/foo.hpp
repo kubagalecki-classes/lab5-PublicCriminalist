0d16a242e09787092645c3ef6f6624215b7f6f17
PublicCriminali <01123869@pw.edu.pl>
2020-12-09 18:13:38 +0000

    Rozwiazanie 2

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -10,8 +10,7 @@ using namespace std;
 vector< char > foo(list< Human >& people)
 {
     vector<char> v;
-    vector<char>::iterator it;
-    for(it=people.begin();it!=people.end();it++){
+    for(auto it=people.begin();it!=people.end();it++){
         it->birthday();
         if(it->isMonster()) v.push_back('n');
         else v.push_back('y');
